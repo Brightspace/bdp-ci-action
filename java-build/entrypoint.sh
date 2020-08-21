d4fda26aee7d241cf131e3af57e4e21f6d54c189
Tim Bai <tim.bai@d2l.com>
2020-08-21 16:00:34 -0400

    setup artifactory access token

--- java-build/entrypoint.sh
+++ java-build/entrypoint.sh
@@ -7,6 +7,10 @@ echo " \n\n === Building .jar file === \n"
 # print maven's version
 mvn --version
 
+# setup Artifactory access token for Maven
+mkdir -p ~/.m2
+echo $1 > ~/.m2/settings.xml
+
 # check if in root directory
 if [ ! -d ".git" ]; then
   echo "You must be in the project's root!\n"
